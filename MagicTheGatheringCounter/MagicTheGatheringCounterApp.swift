0e81b67efb6f73d573a51576c601b6c7665d95eb
Yann Durand <yanndurand.contact@gmail.com>
2023-09-06 18:03:16 +0200

    Initial Commit

---
+++ MagicTheGatheringCounter/MagicTheGatheringCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicTheGatheringCounterApp.swift
+//  MagicTheGatheringCounter
+//
+//  Created by Yann Durand on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MagicTheGatheringCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
